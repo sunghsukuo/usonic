0dae7baa2adcaaae5b7ea8bfb48e1c643ebfe3e2
sunghsukuo <sunghsukuo@gmail.com>
2018-06-16 14:17:08 +0800

    first version

--- usonic.js
+++ usonic.js
@@ -1,1 +1,13 @@
-afsaf
+let usonic = require('mmm-usonic');
+
+usonic.init(function (error) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var sensor = usonic.createSensor(24, 23, 750);
+                    console.log(sensor());
+                }
+            }
+);
+
+
